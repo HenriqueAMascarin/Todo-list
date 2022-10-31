5490cde089165dbcb854b301bf4df7ba971a24d9
LuckTheGamers <thegamers056@gmail.com>
2022-10-31 20:44:19 -0300

    changing the code and pratice

--- src/site/components/Main.jsx
+++ src/site/components/Main.jsx
@@ -1,22 +1,39 @@
-import { useEffect } from "react";
+import React, { useEffect } from "react";
 
 function Main(){
+    let arr = [];
+    let generated = 0;
+
+    function createDiv(){
+        generated = (
+            <div className="task-div">
+            <input type="checkbox" className="button"/>
+            <p>{document.querySelector(".main__form input").value}</p>
+            <button className="button cross"></button>
+            </div>
+        )
+    }
+
+    function task(){
+        createDiv();
+        arr.push(generated);
+        console.log(arr);
+    }
+    
 
     return(
         <main>
             <div className="border content-main">
                 <form className="main__form">
-                    <button type="button" className="button"></button>
+                    <button type="button" onClick={() => task()} className="button"></button>
                     <input type="text" placeholder="Create a new todo..."/>
                 </form>
 
                 <div className="items-content">
 
-                    <div className="task-div">
-                        <input type="checkbox" className="button"/>
-                        <p>{change}</p>
-                        <button className="button cross"></button>
-                    </div>
+                    {useEffect(() => {
+                        <h1>oi</h1>
+                    },arr)}
 
                     <div className="items__info items-color">
                         <p>{} items left</p>
