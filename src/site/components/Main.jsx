e2bf93d9a8d137a68352ed288db7a9959513d714
LuckTheGamers <thegamers056@gmail.com>
2022-11-02 20:41:12 -0300

    add tasks function complete

--- src/site/components/Main.jsx
+++ src/site/components/Main.jsx
@@ -4,7 +4,6 @@ import { scryRenderedComponentsWithType } from "react-dom/test-utils";
 
 function Main(){
     let generated = null;
-    let arr2 = [];
     function createDiv(){
         generated = (
             <div key={Math.random()} className="task-div">
@@ -13,40 +12,35 @@ function Main(){
             <button className="button cross"></button>
             </div>
         )
-        arr2.unshift(generated);
-        console.log(arr2);
-        change(arr2);
     }
     
     const [arr, change] = useState([]);
 
-    // function task(){
-        
-    //     createDiv();
-    //     change([generated]);
-    //     console.log(arr)
-    // }
     function task(){
-        createDiv();
-        
-
+        if(document.querySelector(".main__form input").value != ''){
+            createDiv();
+            change([...arr,generated]);
+            console.log(arr);
+        }
     }
-    
         
     return(
         <main>
             <div className="border content-main">
-                <form className="main__form">
-                    <button type="button" onClick={() => {
-                        task()
-                    } } className="button"></button>
-                    <input type="text" placeholder="Create a new todo..."/>
+                <form className="main__form" onSubmit={(e) => e.preventDefault()}>
+                    <button type="button" onClick={() => task()} className="button"></button>
+                    <input type="text" onKeyDown={(e) => {
+                        if(e.key === "Enter"){
+                            task();
+                        }
+
+                    }} placeholder="Create a new todo..."/>
                 </form>
 
                 <div className="items-content">
                     {arr}
                     <div className="items__info items-color">
-                        <p>{} items left</p>
+                        <p>{arr.length} items left</p>
                         <p>Clear Completed</p>
                     </div>
 
