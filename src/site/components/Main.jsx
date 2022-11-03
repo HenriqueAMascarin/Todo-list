e403e19ccf608231fdcea55402e891e218d3c998
LuckTheGamers <thegamers056@gmail.com>
2022-11-03 14:58:42 -0300

    testing remove elements

--- src/site/components/Main.jsx
+++ src/site/components/Main.jsx
@@ -1,29 +1,38 @@
-import React, { useEffect } from "react";
-import { useState } from "react";
-import { scryRenderedComponentsWithType } from "react-dom/test-utils";
+import React, {useState} from "react";
+import { useEffect } from "react";
 
-function Main(){
+export default function Main(){
     let generated = null;
+    let [arr, change] = useState([]);
+
     function createDiv(){
         generated = (
-            <div key={Math.random()} className="task-div">
+            <div key={Math.random()} name={Math.random()} className="task-div">
             <input type="checkbox" className="button"/>
             <p>{document.querySelector(".main__form input").value}</p>
-            <button className="button cross"></button>
+            <button className="button cross" onClick={(e) => removeElement(e.target.parentNode.getAttribute("name"))}></button>
             </div>
         )
     }
+
+    let [lenghtArray, changeLength] = useState(0);
+    useEffect(() =>{
+        changeLength(arr.length);
+        console.log(arr)
+    },[arr])
     
-    const [arr, change] = useState([]);
+    const removeElement = (e) => {
+        console.log(e);
+        change(arr.filter(item => item.name !== e))
+    }
 
     function task(){
-        if(document.querySelector(".main__form input").value != ''){
+        if(document.querySelector(".main__form input").value !== ''){
             createDiv();
             change([...arr,generated]);
-            console.log(arr);
         }
     }
-        
+    
     return(
         <main>
             <div className="border content-main">
@@ -40,7 +49,7 @@ function Main(){
                 <div className="items-content">
                     {arr}
                     <div className="items__info items-color">
-                        <p>{arr.length} items left</p>
+                        <p>{lenghtArray} items left</p>
                         <p>Clear Completed</p>
                     </div>
 
@@ -55,5 +64,3 @@ function Main(){
         </main>
     );
 }
-
-export default Main;
